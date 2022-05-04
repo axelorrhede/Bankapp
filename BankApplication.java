8f56a5c2d83fea606e6b46ca5d006f7633eb33b2
EmiSve <em0537sv-s@student.lu.se>
2022-05-04 16:04:22 +0200

    Update BankApplication.java
    Påbörjat klassen bankApp.

--- BankApplication.java
+++ BankApplication.java
@@ -1,22 +1,86 @@
+import java.util.Scanner;
 
 public class BankApplication {
-	
+
+	private static Scanner sc;
 
 	public static void main(String[] args) {
-		Bank mybank = new Bank("Swedbank");
-		System.out.println("Add account " + mybank.addAccount("Eva", 7474783));
-		System.out.println("Find Holder " + mybank.findHolder(7474783));
-		System.out.println("Get All accounts " + mybank.getAllAccounts());
-		System.out.println("Get accountnumber " + mybank.findAccountsForHolder(7474783).get(0).getAccountNumber());
-		//Hur man hittar ett accountnumber: mybank.findAccountsForHolder(7474783).get(0).getAccountNumber()
-		System.out.println("Find by number:  "+ mybank.findByNumber(mybank.findAccountsForHolder(7474783).get(0).getAccountNumber()));
-		
-		System.out.println("Find by part of name:  "+ mybank.findByPartofName("EV"));
+		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Remove account "+ mybank.removeAccount(mybank.findAccountsForHolder(7474783).get(0).getAccountNumber()));
+		System.out.println("Välkommen till Orre & Svennis Bankapplikation");
+		System.out.println("Vad önskar ni göra?");
 
-		
+		while (true) {
+			intro();
+			int val = sc.nextInt();
+
+			try {
+
+				if (val > 8 || val < 1) {
+					System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				}
+
+				if (val == 1) {
+					getBankAccount();
+
+				}
+				if (val == 2) {
+					findNågot();
+				}
+				
+				if (val == 3) {
+					
+				}
+				
+				if (val == 4) {
+					
+				}
+				
+				if (val == 5) {
+					
+				}
+				
+				if (val == 6) {
+					
+				}
+				
+				if (val == 7) {
+					
+				}
+				
+				if (val == 8) {
+					
+				}
+				
+				if (val == 9) {
+					
+				}
+
+			} catch (Exception e) {
+				System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				continue; 
+			}
+
+		}
+
+	}
+
+	public static void intro() {
+		System.out.println("Tryck 1 för att leta upp ett konto för en viss kontoinnehavare");
+		System.out.println("Tryck 2 för att leta upp en kontoinnehavare");
+		System.out.println("Tryck 3 för att sätta in pengar på ett nytt konto");
+		System.out.println("Tryck 4 för att ta ut pengar från ett konto");
+		System.out.println("Tryck 5 för att överföra pengar från ett konto till ett annat");
+		System.out.println("Tryck 6 för att skapa ett nytt konto");
+		System.out.println("Tryck 7 för att ta bort ett konto");
+		System.out.println("Tryck 8 för att för att avsluta programmet");
+
+	}
+	
+	public static BankAccount getBankAccount() {
 		
 	}
+	
+	
 
 }
