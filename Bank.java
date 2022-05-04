eac60b607e4309f87f8fa7aa75ba8102aeba809a
axelorrhede <104891812+axelorrhede@users.noreply.github.com>
2022-05-04 12:00:23 +0200

    bank
    bank

--- Bank.java
+++ Bank.java
@@ -1,4 +1,102 @@
+import java.util.ArrayList;
 
 public class Bank {
+	private ArrayList<BankAccount> BankAccounts;
+
+	public Bank() {
+		BankAccounts = new ArrayList<BankAccount>();
+	}
+
+	public int addAccount(String holderName, long idNr) {
+		for (BankAccount account : BankAccounts) {
+			if (account.isHolder(idNr)) {
+				return account.getAccountNumber();
+			}
+		}
+		BankAccount newAccount = new BankAccount(holderName, idNr);
+		BankAccounts.add(newAccount);
+		return newAccount.getAccountNumber();
+		
+	}
+
+	public Customer findHolder(long idNr) {
+		for (BankAccount account : BankAccounts) {
+			if (account.isHolder(idNr)) {
+				return account.getHolder();
+			}
+		}
+		return null;
+	}
+
+	public boolean removeAccount(int number) {
+		for (BankAccount account : BankAccounts) {
+			if (account.getAccountNumber() == number) {
+				BankAccounts.remove(account);
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	ArrayList<BankAccount> getAllAccounts() {
+		BankAccount account;
+		for (int i = 0; i < BankAccounts.size(); i++) {
+			for (int k = i; k < BankAccounts.size(); k++) { // sus
+				if (compareStrings(BankAccounts.get(k).getHolder().getString(),
+						BankAccounts.get(i).getHolder().getString())) {
+					account = BankAccounts.get(k);
+					BankAccounts.remove(k);
+					BankAccounts.add(i, account);
+				}
+			}
+		}
+		return BankAccounts;
+	}
+
+	private boolean compareStrings(String p1, String p2) {
+		if (p1.length() < p2.length()) {
+			for (int i = 0; i < p1.length(); i++) {
+				if (p1.charAt(i) < p2.charAt(i)) {
+					return true;
+				}
+				if (p1.charAt(i) > p2.charAt(i)) {
+					return false;
+
+				}
+			}
+			return true;
+		} else {
+			for (int i = 0; i < p2.length(); i++) {
+				if (p1.charAt(i) < p2.charAt(i)) {
+					return true;
+				}
+				if (p1.charAt(i) > p2.charAt(i)) {
+					return false;
+
+				}
+			}
+			return false;
+		}
+	}
+
+	public BankAccount findByNumber(int accountNumber) {
+		for (BankAccount account : BankAccounts) {
+			if (account.getAccountNumber() == accountNumber) {
+				return account;
+			}
+		}
+		return null;
+	}
+
+	ArrayList<BankAccount> findAccountsForHolder(long idNr){
+		ArrayList<BankAccount> sameCustomer= new ArrayList<BankAccount>();
+		for (BankAccount account : BankAccounts) {
+			if (account.isHolder(idNr)) {
+				sameCustomer.add(account);
+			}
+		}
+		return sameCustomer;
+	}
 
 }
