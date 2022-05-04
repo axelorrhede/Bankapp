0f58687b8ffc47e3183cae5d4b1e1faf8ebac65b
EmiSve <em0537sv-s@student.lu.se>
2022-05-04 11:53:49 +0200

    Update BankAccount.java
    Ny bankkonto class

--- BankAccount.java
+++ BankAccount.java
@@ -1,4 +1,84 @@
+import java.util.Random;
 
 public class BankAccount {
+
+	private double amount;
+	private int kontoId;
+	private Random rand = new Random();
+	private Customer holder; 
+
+	/**
+	 * Skapar ett nytt bankkonto åt en innehavare med namn 'holderName' och id
+	 * 'holderId'. Kontot tilldelas ett unikt kontonummer och innehåller
+	 * inledningsvis 0 kr.
+	 */
+	public BankAccount(String holderName, long holderId) {
+		Customer ny = new Customer(holderName, holderId);
+		holder = ny; 
+		amount = 0;
+		kontoId = rand.nextInt(Integer.MAX_VALUE - 1000) + 1000;
+
+	}
+
+	/**
+	 * Skapar ett nytt bankkonto med innehavare 'holder'. Kontot tilldelas ett unikt
+	 * kontonummer och innehåller inledningsvis 0 kr.
+	 */
+
+	public BankAccount(Customer holder) {
+		holder.getcustomerNbr();
+		holder.getidNr();
+		holder.getString();
+		this.holder = holder;
+		
+		kontoId = rand.nextInt(Integer.MAX_VALUE - 1000) + 1000;
+		amount = 0;
+
+	}
+
+	/** Tar reda på kontots innehavare. */
+	public Customer getHolder() {
+		return this.holder;
+				
+	}
+
+	/** Tar reda på det kontonummer som identifierar detta konto. */
+
+	public int getAccountNumber() {
+		return kontoId;
+
+	}
+
+	/** Tar reda på hur mycket pengar som finns på kontot. */
+	public double getAmount() {
+		return amount;
+
+	}
+
+	/** Sätter in beloppet 'amount' på kontot. */
+	public void deposit(double amount) {
+		this.amount += amount;
+
+	}
+
+	/**
+	 * Tar ut beloppet 'amount' från kontot. Om kontot saknar täckning blir saldot
+	 * negativt.
+	 */
+	public void withdraw(double amount) {
+		this.amount -= amount;
+
+	}
+
+	/** Returnerar en strängrepresentation av bankkontot. */
+	public String toString() {
+		return "Saldo: " + amount + "\n Kontoid :" + kontoId + "\n" + holder.getString();  
+
+	}
+	
+	public boolean isHolder(long idNbr) {
+		return (idNbr == holder.getcustomerNbr());
+		
+	}
 
 }
