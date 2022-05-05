10b83bbf1ed2475e9e126dffd2d092ceb239a51a
axelorrhede <104891812+axelorrhede@users.noreply.github.com>
2022-05-05 11:26:09 +0200

    MAnnen de e bra nu

--- BankApplication.java
+++ BankApplication.java
@@ -1,64 +1,60 @@
-import java.util.InputMismatchException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BankApplication {
 
 	private static Scanner sc;
-	private static Bank bank;
+	private Bank bank;
+	private int sum; 
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
+	public BankApplication() {
+		sc = new Scanner(System.in);
 		System.out.println("Välkommen till Orre & Svennis Bankapplikation");
 		System.out.println("Vad vill ni döpa er bank till?");
-		Bank bank = new Bank(sc.next());
+		bank = new Bank(sc.nextLine());
 		System.out.println("Välkommen till " + bank.toString());
 		System.out.println("Vad önskar ni göra?");
-
-		while (true) {
+		
+		int val;
+			boolean kor = true;  // Boolean för att köra programmet 
+		while (kor) {
 			intro();
-			int val = 0; 
-			try {
-	
-				 val = sc.nextInt();
-			
+			val = scanner(8);
+			System.out.println("Du valde val: " + val);
 
-				switch (val) {
-				case 1:
-					getBankAccount();
-				case 2:
-					findCustomer();
-				case 3:
-					deposit();
-				case 4:
-					withdraw();
-				case 5:
-					transfer();
-				case 6:
-					createAccount();
-				case 7:
-					removeAccount();
-				case 8:
-					System.out.println("Bankprogrammet avslutas."); // Lägg till "sparar" promt här
-					break;
-				default:
-					System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
-					continue;
-				}
-
-			} catch (InputMismatchException e) {
-				System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen"); 
-				continue;
-				
-
-			} finally {
-				sc.nextLine(); //För att fånga upp retur-knappen
+			switch (val) {
+			case 1:
+				getBankAccount();
+				break;
+			case 2:
+				findCustomer();
+				break;
+			case 3:
+				deposit();
+				break;
+			case 4:
+				withdraw();
+				break;
+			case 5:
+				transfer();
+				break;
+			case 6:
+				createAccount();
+				break;
+			case 7:
+				removeAccount();
+				break;
+			case 8:
+				System.out.println("Bankprogrammet avslutas."); // Lägg till "sparar" promt här
+				kor = false; 
+				break;
 			}
+
 		}
 
 	}
 
-	public static void intro() {
+	public void intro() {
 		System.out.println("Tryck 1 för att leta upp ett konto för en viss kontoinnehavare");
 		System.out.println("Tryck 2 för att leta upp en kontoinnehavare");
 		System.out.println("Tryck 3 för att sätta in pengar på ett nytt konto");
@@ -70,51 +66,150 @@ public class BankApplication {
 
 	}
 
-	public static BankAccount getBankAccount() {
-		System.out.println("Vill ni söka på kundnummer eller personnummer?");
-		System.out.println("Tryck 1 för kundnummer och 2 för personnummer.");
-		int val = sc.nextInt();
+	public BankAccount getBankAccount() {
+		System.out.println("Ange personnummer: ");
+		int idNr = scanner(111111111);
+		ArrayList<BankAccount> accounts = bank.findAccountsForHolder(idNr);
+		System.out.println("Välj ett konto: ");
+		for (int i = 0; i < accounts.size(); i++) {
+			System.out.println("Skriv " + i + " för konto: " + accounts.get(i));
+
+		}
+		return accounts.get(scanner(accounts.size()));
+
+	}
+
+	public Customer findCustomer() {
+		System.out.println("Mata in vilken person du vill söka efter");
+		String person = scanner();
+		System.out.println("Vem av dessa personer? " + bank.findByPartofName(person));
+		ArrayList<Customer> customers = bank.findByPartofName(person);
+		for (Customer customer: customers) {
+			sum++;
+			System.out.println("val " + sum + customer);
+		}
+		return customers.get(scanner(customers.size()));
+
+	}
+
+	public void deposit() {
+		BankAccount account = getBankAccount();
+		account.deposit(scanner(Double.MAX_VALUE));
+		System.out.println("Detta konto har: " + account.getAmount() + " SEK");
+
+	}
+
+	public void withdraw() {
+		BankAccount account = getBankAccount();
+		account.withdraw(scanner(account.getAmount() + 1000.0)); // Max back 1000 kr
+		System.out.println("Detta konto har: " + account.getAmount() + " SEK");
+
+	}
+
+	public void transfer() {
+		BankAccount account = getBankAccount();
+		System.out.println("Detta konto har: " + account.getAmount() + " SEK");
+		double value = scanner(account.getAmount() + 1000.0);
+		account.withdraw(scanner(account.getAmount() + 1000.0));
+		BankAccount account2 = getBankAccount(); // Nytt kontoobjekt där man lägger till pengar
+		account2.deposit(value);
+		System.out.println("Detta konto har: " + account2.getAmount() + " SEK");
+
+	}
+
+	public void createAccount() {
+		System.out.println("Ange namn: ");
+		String name = scanner();
+		System.out.println("Ange Personnummer: ");
+		Long idNr = scanner(Long.MAX_VALUE);
+		bank.addAccount(name, idNr);
+		System.out.println("Konto för " + name + " med personnummer: " + idNr + " har nu skapats");
+
+	}
+
+	public void removeAccount() {
+
+	}
+
+	public int scanner(int goodNumbers) {
 		while (true) {
-			switch (val) {
-			case 1: // Kod för att söka på kundnummer;
-				break;
-			case 2: // Kod för att söka på personnummer;
-				break;
-			default:
+			try {
+				int val = 0;
+				val = Integer.parseInt(sc.nextLine());
+				if (val >= 0 && val <= goodNumbers) {
+					return val;
+				} else {
+					System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				}
+
+			} catch (NumberFormatException e) {
 				System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
 				continue;
 
+				// } finally {
+				// sc.nextLine(); // För att fånga upp retur-knappen
 			}
 		}
-
 	}
 
-	public static Customer findCustomer() {
+	public double scanner(double goodNumbers) {
+		while (true) {
+			try {
+				double val = 0;
+				val = Double.parseDouble(sc.nextLine());
+				if (val >= 0 && val <= goodNumbers) {
+					return val;
+				} else {
+					System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				}
 
-		System.out.println("Mata in vilken person du vill söka efter");
-		String person = sc.next();
-		System.out.println("Vem av dessa personer? " + bank.findByPartofName(person));
+			} catch (NumberFormatException e) {
+				System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				continue;
 
+				// } finally {
+				// sc.nextLine(); // För att fånga upp retur-knappen
+			}
+		}
 	}
 
-	public static void deposit() {
+	public String scanner() {
+		while (true) {
+			try {
+				String val;
+				val = sc.nextLine();
+				return val;
+			}
 
+			catch (NumberFormatException e) {
+				System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				continue;
+
+				// } finally {
+				// sc.nextLine(); // För att fånga upp retur-knappen
+			}
+		}
 	}
 
-	public static void withdraw() {
+	public long scanner(long goodNumbers) {
+		while (true) {
+			try {
+				long val = 0;
+				val = Long.parseLong(sc.nextLine());
+				if (val >= 0 && val <= goodNumbers) {
+					return val;
+				} else {
+					System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				}
 
-	}
+			} catch (NumberFormatException e) {
+				System.out.println("Detta är dessvärre inte ett giltigt val. Försök igen");
+				continue;
 
-	public static void transfer() {
-
-	}
-
-	public static void createAccount() {
-
-	}
-
-	public static void removeAccount() {
-
+				// } finally {
+				// sc.nextLine(); // För att fånga upp retur-knappen
+			}
+		}
 	}
 
 }
